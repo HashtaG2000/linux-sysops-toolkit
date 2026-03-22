15632b19e45bf94ef7b48f74903c030e85919980
HashtaG2000 <varmaratnesh25@gmail.com>
2026-03-22 16:04:30 +0100

    Added if else statement to memory command

--- health_check.sh
+++ health_check.sh
@@ -13,7 +13,16 @@ echo ""
 echo "$DATE"
 
 echo "Memory"
-free -h
+AVAILABLE_MEM=$(free -m | grep Mem | awk '{print $7}')
+
+#If else loop for memery status
+if [ "$AVAILABLE_MEM" -lt 500 ]; then
+    echo "STATUS: WARNING - Low memory ($AVAILABLE_MEM MB)"
+elif [ "$AVAILABLE_MEM" -lt 1000 ]; then
+    echo "STATUS: CAUTION - Available memory is under 1GB ($AVAILABLE_MEM MB)"
+else
+    echo "STATUS: OK ($AVAILABLE_MEM MB)"
+fi
 echo ""
 
 echo "Disk"
